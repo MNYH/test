87ae93f0ca19ec9f42272d83b08c983913c43159
MNYH <thelghks@naver.com>
2024-12-27 15:29:31 +0900

    Sreamlit 실습

---
+++ my_page_main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('로또 당첨 번호 만들기')
+
+df = pd.DataFrame(np.random.randint(1, 46, size = (5, 6)), columns = ["1번째", "2번째","3번째", "4번째","5번째", "보너스 번호"])
+
+st.write(df)
